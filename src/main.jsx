c375fc096282be7ff54df7c83bdb77e1c1546f21
Arsen Zhetibayev <zhetibayevarsen@gmail.com>
2026-04-10 23:40:48 +0500

    Add missing import for React in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import { LanguageProvider } from './contexts/LanguageContext';
 import { ThemeProvider } from './contexts/ThemeContext';
 import './styles/global.css';
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
